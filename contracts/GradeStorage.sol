af6c2c7f026e0bb2ee74633cb3739306994a7ff2
suraj <surajj3404@gmail.com>
2023-12-11 20:54:08 +0530

    Update getGPA

--- contracts/GradeStorage.sol
+++ contracts/GradeStorage.sol
@@ -1,42 +1,42 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
-contract GradeStorage {
-    uint256 public totalCredits;
-    uint256 public totalGradePoints;
-    uint256 public gpa;
-    mapping(string => uint256) public courseToGrade;
-    struct Course {
-        string code;
-        uint256 grade;
-        uint256 credits;
-    }
-    Course[] public Courses;
-
-    function updateGPA() public {
-        if(Courses.length == 0){
-            gpa = 0;
-            return;
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.8;
+    contract GradeStorage {
+        uint256 public totalCredits;
+        uint256 public totalGradePoints;
+        uint256 public gpa;
+        mapping(string => uint256) public courseToGrade;
+        struct Course {
+            string code;
+            uint256 grade;
+            uint256 credits;
         }
-        for (uint256 i = 0; i < Courses.length; i++) {
-            totalCredits += Courses[i].credits;
-            totalGradePoints += Courses[i].grade * Courses[i].credits;
+        Course[] public Courses;
+
+        function updateGPA() public {
+            if(Courses.length == 0){
+                gpa = 0;
+                return;
+            }
+            for (uint256 i = 0; i < Courses.length; i++) {
+                totalCredits += Courses[i].credits;
+                totalGradePoints += Courses[i].grade * Courses[i].credits;
+            }
+            gpa = totalGradePoints * 100000/ totalCredits;
         }
-        gpa = totalGradePoints / totalCredits;
-    }
 
-    //call data and memory only stores the data for a very little amount of time
-    //the only diff here is that the memory gives us the permission to modify the data
-    function addGrade(
-        string memory _code,
-        uint256 _grade,
-        uint256 _credits
-    ) public {
-        Courses.push(Course(_code, _grade, _credits));
-        courseToGrade[_code] = _grade;
-        updateGPA();
-    }
+        //call data and memory only stores the data for a very little amount of time
+        //the only diff here is that the memory gives us the permission to modify the data
+        function addGrade(
+            string memory _code,
+            uint256 _grade,
+            uint256 _credits
+        ) public {
+            Courses.push(Course(_code, _grade, _credits));
+            courseToGrade[_code] = _grade;
+            updateGPA();
+        }
 
-    function getGPA() public view returns (uint256) {
-        return gpa;
+        function getGPA() public view returns (uint256) {
+            return (gpa * 100) / 100000;
+        }
     }
-}
