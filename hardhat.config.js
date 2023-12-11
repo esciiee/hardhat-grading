57889880462b30742f7237e6201e3c5284cdcfe4
suraj <surajj3404@gmail.com>
2023-12-11 20:02:16 +0530

    Add sepolia test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("./tasks/block-number")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+
 module.exports = {
-  solidity: "0.8.8",
-};
+    solidity: "0.8.8",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+}
