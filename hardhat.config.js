3ddb837086c42ae8b8d190c74c33d9025a604c44
suraj <surajj3404@gmail.com>
2023-12-12 00:34:55 +0530

    Add Gas Reports

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("./tasks/block-number")
 require("dotenv").config()
+require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
-
 
 module.exports = {
     solidity: "0.8.8",
@@ -16,6 +16,14 @@ module.exports = {
         localhost: {
             url: process.env.LOCALHOST_RPC_URL,
             chainId: 31337,
-        }
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+        token: "MATIC"
     },
 }
--- test/test-deploy.js
+++ test/test-deploy.js
@@ -11,16 +11,14 @@ describe("GradeStorage", function () {
     it("Should start with a GPA of 0", async function () {
         const currentGPA = await gradeStorage.getGPA();
         const expectedGPA = "0"
-        assert.equal(currentGPA.toString(), expectedGPA.toString());
+        assert.equal(currentGPA.toString(), expectedGPA);
     })
 
     it("Should add a grade", async function () {
         const transaction = await gradeStorage.addGrade("APL107", 7, 4)
         await transaction.wait()
-        const transaction2 = await gradeStorage.addGrade("APL108", 8, 4)
-        await transaction2.wait()
         const newGPA = await gradeStorage.getGPA()
-        const expectedGPA = 750
-        assert.equal(newGPA.toString(), expectedGPA.toString())
+        const expectedGPA = "700"
+        assert.equal(newGPA.toString(), expectedGPA);
     })
 })
